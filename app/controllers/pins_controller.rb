a579fde8e647fd17ff37a6d9124d9eccf8665a25
Justin Veirs <justinveirs@gmail.com>
2014-08-04 17:40:05 -0600

    updated before actions, and actual actions.

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,7 @@
 class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     @pins = Pin.all
@@ -43,7 +44,11 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
+    def correct_user
+      @pin = current_user.pins.find_by(:id params[:id])
+      redirect_to pins_path, notice: "not authorized to edit this pin" if @pin.nil?
+
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
