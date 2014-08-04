d64dd059925f45ea4e51b591da670c03b6140fe8
Justin Veirs <justinveirs@gmail.com>
2014-08-04 16:30:03 -0600

    adjusting pins controller to reflect tutorial

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -10,44 +10,32 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params)
-
-    respond_to do |format|
-      if @pin.save
-        format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
-        format.json { render :show, status: :created, location: @pin }
-      else
-        format.html { render :new }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
-      end
+    @pin = current_user.pins.build(params)
+    if @pin.save
+      redirect_to @pin, notice: 'Pin Created, Yo!'
+    else
+      render_new
     end
   end
 
   def update
-    respond_to do |format|
-      if @pin.update(pin_params)
-        format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
-        format.json { render :show, status: :ok, location: @pin }
-      else
-        format.html { render :edit }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
-      end
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated'
+    else
+      render_edit
     end
   end
 
   def destroy
     @pin.destroy
-    respond_to do |format|
-      format.html { redirect_to pins_url, notice: 'Pin was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to pins_path
   end
 
   private
